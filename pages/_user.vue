c07afb104afc6d2bf82e64bfa2ce1fab85ccfcb4
Alexey <a.morozov@hackspace.by>
2020-02-07 17:14:56 +0300

    User page

---
+++ pages/_user.vue
@@ -0,0 +1,54 @@
+<template>
+  <v-layout>
+    <v-flex xs12 sm8 md6>
+      <h1 class="display-1 pb-5">User #{{ this.$route.params.user }}</h1>
+      <v-card max-width="400">
+        <v-card-title>{{user.name}}</v-card-title>
+        <v-card-text>
+          <div class="subtitle-1 black--text">
+            {{ user.email }}
+          </div>
+          <div>{{ user.phone }} <a :href="'http://' + user.website" target="_blank" rel="nofollow">{{ user.website }}</a></div>
+        </v-card-text>
+        <v-divider class="mx-4"></v-divider>
+        <v-card-title>Address</v-card-title>
+        <v-card-text>
+          <v-list-item two-line>
+            <v-list-item-content>
+              <v-list-item-title>{{ user.address.street}}</v-list-item-title>
+              <v-list-item-subtitle>Street</v-list-item-subtitle>
+            </v-list-item-content>
+            <v-list-item-content>
+              <v-list-item-title>{{ user.address.suite}}</v-list-item-title>
+              <v-list-item-subtitle>Suite</v-list-item-subtitle>
+            </v-list-item-content>
+          </v-list-item>
+          <v-list-item>
+            <v-list-item-content>
+              <v-list-item-title>{{ user.address.city}}</v-list-item-title>
+              <v-list-item-subtitle>City</v-list-item-subtitle>
+            </v-list-item-content>
+            <v-list-item-content>
+              <v-list-item-title>{{ user.address.zipcode}}</v-list-item-title>
+              <v-list-item-subtitle>Zipcode</v-list-item-subtitle>
+            </v-list-item-content>
+          </v-list-item>
+        </v-card-text>
+        <v-card-actions class="pa-5">
+          <v-btn class="px-5" color="grey" dark>Posts</v-btn>
+          <v-btn class="px-5" color="grey" dark>Albums</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+  export default {
+    async asyncData ({$axios, params, error}) {
+      const user = await $axios.$get('https://jsonplaceholder.typicode.com/users/' + params.user)
+      return {user}
+    },
+    data: () => ({})
+  }
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -2,6 +2,34 @@
   <v-layout>
     <v-flex xs12 sm8 md6>
       <h1 class="display-1">Users</h1>
+      <v-card class="my-5" max-width="400" tile>
+        <v-list>
+          <v-list-item-group color="primary">
+            <v-list-item v-for="user in users" :key="user.id" :to="'/' + user.id">
+              <v-list-item-content>
+                <v-list-item-title>{{ user.name }}</v-list-item-title>
+                <v-list-item-subtitle>{{ user.email }}</v-list-item-subtitle>
+              </v-list-item-content>
+            </v-list-item>
+          </v-list-item-group>
+        </v-list>
+      </v-card>
     </v-flex>
   </v-layout>
 </template>
+
+<script>
+  export default {
+    asyncData ({$axios, error}) {
+      return $axios.$get('https://jsonplaceholder.typicode.com/users')
+        .then(users => {
+          return {
+            users
+          }
+        }).catch(e => {
+          error(e)
+        })
+    },
+    data: () => ({})
+  }
+</script>
